3143f0da85340b27f23a01806c7c5ab30f7594d1
GeonwooVincentKim <kdsnop@gmail.com>
2023-06-16 17:12:29 +0900

    [Feature] - Generate login.dart page

---
+++ lib/login.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({super.key});
+
+  @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 40),
+          TextField(
+            controller: emailController,
+            cursorColor: Colors.white,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(labelText: "Enter the email")
+          ),
+          SizedBox(height: 4),
+          TextField(
+            controller: passwordController,
+            cursorColor: Colors.white,
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(labelText: "Password"),
+            obscureText: true,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size.fromHeight(50),
+            ),
+            icon: Icon(Icons.lock_open, size: 32),
+            label: Text(
+              'Sign In',
+              style: TextStyle(fontSize: 24),
+            ),
+            onPressed: () {},
+          )
+        ],
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:first_firebase_project/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -24,43 +25,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  Future<void> initFirebaseMessaging() async {
-    print(await FirebaseMessaging.instance.getInitialMessage());
-    FirebaseMessaging.onMessage.listen((message) {
-      if (message.notification != null) {
-        print(message.notification!.body);
-        print("????");
-        print(message.notification!.title);
-      } else {
-        print("null~!!");
-      }
-    });
-
-    print("Test!");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    print(FirebaseMessaging.instance.pluginConstants);
-    print("Check -> ${FirebaseMessaging.instance.isSupported()}");
-    initFirebaseMessaging();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("Hi"),
-      ),
+      body: LoginWidget(),
     );
   }
 }
