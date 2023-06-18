70f3b5c3c3685ee6597d38a4450b94d24fcb434d
GeonwooVincentKim <kdsnop@gmail.com>
2023-06-18 09:23:30 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:first_firebase_project/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
